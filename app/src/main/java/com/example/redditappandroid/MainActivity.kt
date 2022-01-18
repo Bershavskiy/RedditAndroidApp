d11a9cfb0cef8e6d8aa4a932b78c2f05b513d89c
Bershavskiy <bershavskij@gmail.com>
2022-01-18 22:30:03 +0200

    Added fetching data from reddit API

--- app/src/main/java/com/example/redditappandroid/MainActivity.kt
+++ app/src/main/java/com/example/redditappandroid/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.redditappandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.example.redditappandroid.databinding.ActivityMainBinding
+import com.example.redditappandroid.models.Post
+import org.json.JSONObject
+import java.io.IOException
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    private val postAdapter = PostAdapter()
+    var postsFromApi =  ArrayList<Post>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+        apiCall(postAdapter)
     }
+
+    private fun apiCall(postAdapter: PostAdapter) {
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://www.reddit.com/top.json?limit=15"
+
+        val stringRequest: JsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                val obj: JSONObject = response
+                try {
+                    val array = obj.getJSONObject("data").getJSONArray("children")
+                    for (i in 0..array.length()-1) {
+                        val curPost: JSONObject = array.getJSONObject(i)
+                        var username = curPost.getJSONObject("data").getString("author")
+                        var created_utc = curPost.getJSONObject("data").getLong("created_utc")
+                        var thumbnail = curPost.getJSONObject("data").getString("thumbnail")
+                        var num_comments = curPost.getJSONObject("data").getInt("num_comments")
+                        postAdapter.addPost(Post(username, created_utc.toLong(), thumbnail, num_comments.toInt()))
+                    }
+                }catch (e: Exception){
+                    Log.e("Error", e.toString())
+                }
+
+            },
+            { e ->
+                Log.e("Error",e.toString())
+            })
+        queue.add(stringRequest)
+    }
+
+     private fun init(){
+         binding.apply {
+             rvPostList.layoutManager = LinearLayoutManager(this@MainActivity)
+             rvPostList.adapter = postAdapter
+         }
+     }
 }
--- app/src/main/java/com/example/redditappandroid/PostAdapter.kt
+++ app/src/main/java/com/example/redditappandroid/PostAdapter.kt
@@ -1,21 +1,41 @@
 package com.example.redditappandroid
 
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.redditappandroid.databinding.PostItemBinding
 import com.example.redditappandroid.models.Post
+import java.io.IOException
+import java.net.URL
+
+import kotlin.collections.ArrayList
 
 class PostAdapter:RecyclerView.Adapter<PostAdapter.PostHolder>() {
+     lateinit var context: Context
     var postList:ArrayList<Post> = ArrayList()
 
     class PostHolder(item: View): RecyclerView.ViewHolder(item) {
         val binding = PostItemBinding.bind(item)
-        fun bind (post: Post){
-            binding.tvAutor.text = post.autor
-            binding.tvDateAdded.text = post.created_utc.toString()
+        fun bind (post: Post) {
+            val hours = ((System.currentTimeMillis()/1000) - post.created_utc.toLong())/60/60
+            binding.tvAutor.text =  "Posted by ${post.autor}"
+            binding.tvDateAdded.text = "${hours} hours ago"
             binding.tvNumbersOfComment.text = post.numberOfComments.toString()
+
+            if(post.thummbnail.substring(0,4)=="http"){
+                binding.imageView2.visibility = View.VISIBLE
+                Glide.with(binding.root).load(post.thummbnail).into(binding.imageView2);
+            }else{
+                binding.imageView2.visibility = View.GONE
+            }
+            binding.imageView2.setImageResource(R.drawable.ic_launcher_background)
         }
     }
 
@@ -26,6 +46,7 @@ class PostAdapter:RecyclerView.Adapter<PostAdapter.PostHolder>() {
 
     override fun onBindViewHolder(holder: PostHolder, position: Int) {
         holder.bind(postList[position])
+        holder.setIsRecyclable(false)
     }
 
     override fun getItemCount(): Int {
